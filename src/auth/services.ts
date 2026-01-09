b923edc36181a863d5be204709a4413bbc895026
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-01-10 08:31:12 +1030

    [auth-services] Add a service to get a user and another to generate refresh token.

---
+++ src/auth/services.ts
@@ -0,0 +1,26 @@
+import { prisma } from '../shared/lib/prisma.js';
+import type { User } from './types.js';
+
+export async function getUser(username: string): Promise<User | null> {
+  return await prisma.user.findFirst({
+    where: {
+      email: username
+    }
+  });
+}
+
+export async function saveRefreshToken(
+  token: string,
+  userId: string
+): Promise<void> {
+  // [x]: You need to change unknown type here
+  // [ ]: Error handling of this function
+  await prisma.user.update({
+    where: {
+      id: userId
+    },
+    data: {
+      refreshToken: token
+    }
+  });
+}
