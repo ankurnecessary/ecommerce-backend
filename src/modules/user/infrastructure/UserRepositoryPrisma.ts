9056c1584eecfdbaf49cdd6a5273b05f37ed7513
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-01-22 04:53:04 +1030

    [user-repository-implementation] Create actual user repository using prisma.

---
+++ src/modules/user/infrastructure/UserRepositoryPrisma.ts
@@ -0,0 +1,23 @@
+import { prisma } from '../../../lib/prisma.js';
+import { User } from '../domain/User.js';
+import UserRepository from '../domain/UserRepository.js';
+
+export const UserRepositoryPrisma: UserRepository = {
+  async findByEmail(email: string) {
+    const record = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
+
+    if (!record) {
+      return null;
+    }
+
+    return User.create({
+      id: record.id,
+      email: record.email,
+      password: record.password
+    });
+  }
+};
