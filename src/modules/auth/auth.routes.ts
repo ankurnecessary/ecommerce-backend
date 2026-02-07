3e4c8cee9020c7cac9e5185c3f41baffce2775e7
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-02-08 05:05:35 +1030

    [refresh-route] Set actual refresh route i.e. /refresh API.

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { loginController, logoutController } from './auth.controller.js';
+import {
+  loginController,
+  logoutController,
+  refreshController
+} from './auth.controller.js';
 
 const router = Router();
 
 router.post('/login', loginController);
 router.post('/logout', logoutController);
+router.post('/refresh', refreshController);
 
 export default router;
