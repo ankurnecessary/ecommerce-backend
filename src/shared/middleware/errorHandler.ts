55413ea032d4965ca950edfdb4c2cddd47380f30
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-02-09 03:57:31 +1030

    [global-error-handling] Rectify global error handler. So that it will not throw HTML but JSON error object.

--- src/shared/middleware/errorHandler.ts
+++ src/shared/middleware/errorHandler.ts
@@ -1,7 +1,12 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { HttpError } from '@/shared/errors/HttpError.js';
 
-export function errorHandler(err: Error, req: Request, res: Response) {
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) {
   if (err instanceof HttpError) {
     return res.status(err.statusCode).json({
       message: err.message
