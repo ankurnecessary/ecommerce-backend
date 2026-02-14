1c0c5fbace6a7be77c584e1974df14a74637bf24
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-02-15 08:28:15 +1030

    [requireAuth-middleware] Add constants for messages needed in requireAuth middleware.

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -3,5 +3,8 @@ export const REGEX = {
 } as const;
 
 export const VALIDATION_MESSAGES = {
-  INVALID_CREDENTIALS: 'Invalid credentials'
+  INVALID_CREDENTIALS: 'Invalid credentials',
+  MISSING_ACCESS_TOKEN: 'Missing access token',
+  INVALID_TOKEN_PAYLOAD: 'Invalid token payload',
+  INVALID_OR_EXPIRED_ACCESS_TOKEN: 'Invalid or expired access token'
 } as const;
