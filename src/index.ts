2507075be4e89581da29945192c080255a4575ad
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-02-19 03:48:03 +1030

    [server-separation][@-path-handling][production-error] Separate development and production server configuration. Resolve @ alias issues in production build.

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,3 @@ import app from './app.js';
 
 // AWS Lambda mode
 export const handler = serverless(app);
-
-if (process.env.NODE_ENV === 'development') {
-  // Local development mode
-  const port = process?.env?.PORT ?? '5000';
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-}
